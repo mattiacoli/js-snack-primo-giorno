2ec8dbfab631fc3b8ecd88356ea8f04a7387cf5c
mattiacoli <mattia.coli@aol.com>
2025-02-13 10:29:49 +0100

    done snack 1

--- snack 1/script.js
+++ snack 1/script.js
@@ -2,3 +2,15 @@
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
+
+
+const numbOne = Number(prompt('Inserisci un numero ') )
+const numbTwo = Number(prompt('Inserisci un altro numero') )
+
+if (numbOne > numbTwo){
+  console.log(numbOne);
+  
+} else {
+  console.log(numbTwo);
+  
+}
